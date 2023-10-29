47c6f3f740d3b5243b01e8d7dd2d7c321ae9f0f9
CMeskita <danielle.cmesquita@gmail.com>
2023-10-29 10:26:16 -0300

    forgout e aaccount

--- app/Acess/account/page.tsx
+++ app/Acess/account/page.tsx
@@ -1,46 +1,53 @@
+"use client"
+import { Login } from "@/app/Login/login";
 import { Button } from "@/app/components/Buttons/Button";
 import { Card } from "@/app/components/Cards/Card";
 import { FormaRedonda } from "@/app/components/Formas/FormaRedonda";
 import { Input } from "@/app/components/Inputs/Input";
-import {InputCheckbox} from "@/app/components/Inputs/InputCheckbox";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
+
 //validando campos
-const createLoginSchema=z.object({
-   email:z.string().email('Formato do email inválido'),
-   password:z.string().min(6,'Senha precisa de no minímo 6 caracteres')
-    })
-//tipando o objeto de validação
-type createLoginFormData=z.infer<typeof createLoginSchema>
-    
-    
-export function Login(){
-    //criando constantes de validação
+const accountLoginSchema=z.object({
+    email:z.string().email('Formato do email inválido'),
+    password:z.string().min(6,'Senha precisa de no minímo 6 caracteres'),
+    confirm:z.string().min(6,'Senha precisa de no minímo 6 caracteres')
+
+     })
+ //tipando o objeto de validação
+ type accountFormData=z.infer<typeof accountLoginSchema>
+
+export default function Account(){
 
     const [output, SetOutput]=useState('')
-    const {register,handleSubmit,formState:{errors}}=useForm<createLoginFormData>({
-          resolver:zodResolver(createLoginSchema)})
-//criando função para receber os dados
-          function acessLogin(data:createLoginFormData ){ 
-            if(data.password=='123456' && data.email=='danielle.cmesquita@gmail.com')
-            {
-            SetOutput(JSON.stringify(data,null, 2))
+    const {register,handleSubmit,formState:{errors}}=useForm<accountFormData>({
+          resolver:zodResolver(accountLoginSchema)})
 
-            }else{
-                SetOutput(JSON.stringify(data,null,"Error"))
-            }
-            
-           }
+function accountLogin(data:accountFormData )
+    { 
+   
+        if(data.password == data.confirm)
+        {
+        SetOutput(JSON.stringify(data,null, 2))
+
+        window.history.go(-2);
+
+        }else{
+            SetOutput(JSON.stringify("Senhas Não conferem",null,2))
+        }
+        
+       }
+
 
     return(
-         
-    <div className='text-center ite sm:text-left min-[320px]:text-center max-[600px]:bg-sky-300'>     
+
+        <div className='text-center ite sm:text-left min-[320px]:text-center max-[600px]:bg-sky-300'>     
         <div className='max-w-sm mx-auto m-10'>
            <Card>
-            <form onSubmit={handleSubmit(acessLogin)} className="space-y-6">
+            <form onSubmit={handleSubmit(accountLogin)} className="space-y-6">
                  <div className="flex just items-center space-x-5">
                     <FormaRedonda>i</FormaRedonda>
                         <h3 className="text-xl font-medium text-gray-900 dark:text-white text-center">Acesso </h3>
@@ -58,7 +65,7 @@ export function Login(){
                             {errors.email && <span>{errors.email.message}</span>}
 
                     </div>
-                <div>
+                    <div>
                         <label className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Sua Senha</label>                            
                             <Input.Field 
                         id='password'
@@ -68,24 +75,28 @@ export function Login(){
                         />
                          {errors.password && <span>{errors.password.message}</span>}
 
-                        </div>
+                    </div>
+                    <div>
+                        <label className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Confimrar Senha</label>                            
+                            <Input.Field 
+                        id='confirm'
+                        type='password'
+                        placeholder="Confirme"
+                        {...register('confirm')}
+                        />
+                         {errors.confirm && <span>{errors.confirm.message}</span>}
+
+                    </div>
                             <div className="flex items-start">
-                                <div className="flex items-start">
-                                   
-                                    </div>
-                                    <a href="/Acess/forgout" className="text-sm text-blue-700 hover:underline ml-auto dark:text-blue-500">Esqueceu a Senha?</a>
-                                </div>
+                             
                                <Button>{'Acessar'}</Button>
-                                <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
-                                    Deseja se Cadastrar? <a href="/Acess/account" className="text-blue-700 hover:underline dark:text-blue-500">
-                                        account</a>
-                        </div>
+                
+                             </div>
                 </form>
                 </Card>
                 <pre className=' text-black'>{output}</pre>
              </div>
        
         </div>
-      
-        )   
+    )
 }
---
+++ app/Acess/forgout/page.tsx
@@ -0,0 +1,76 @@
+"use client"
+import { Button } from "@/app/components/Buttons/Button"
+import { Card } from "@/app/components/Cards/Card"
+import { FormaRedonda } from "@/app/components/Formas/FormaRedonda"
+import { Input } from "@/app/components/Inputs/Input"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useState } from "react"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+
+//validando campos
+const forgoutLoginSchema=z.object({
+    email:z.string().email('Formato do email inválido'),
+
+     })
+ //tipando o objeto de validação
+ type forgoutFormData=z.infer<typeof forgoutLoginSchema>
+export default function Forgout(){
+  //criando constantes de validação
+
+  const [output, SetOutput]=useState('')
+  const {register,handleSubmit,formState:{errors}}=useForm<forgoutFormData>({
+        resolver:zodResolver(forgoutLoginSchema)})
+
+
+    function forgoutLogin(data:forgoutFormData ){ 
+   
+            SetOutput(JSON.stringify(data,null,"Error"))
+    
+        
+       }
+
+
+    return(
+        <div className='text-center ite sm:text-left min-[320px]:text-center max-[600px]:bg-sky-300'>     
+        <div className='max-w-sm mx-auto m-10'>
+           <Card>
+            <form onSubmit={handleSubmit(forgoutLogin)} className="space-y-6">
+                 <div className="flex just items-center space-x-5">
+                    <FormaRedonda>i</FormaRedonda>
+                        <h3 className="text-xl font-medium text-gray-900 dark:text-white text-center">Acesso </h3>
+                    
+                 </div>
+                    <div>  
+                        <label  className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Seu email</label>
+                            
+                            <Input.Field 
+                            id="email"
+                            type='email'
+                            placeholder="Digite email"
+                            {...register('email')}
+                            />
+                            {errors.email && <span>{errors.email.message}</span>}
+
+                    </div>
+             
+                            <div className="flex items-start">
+                                <div className="flex items-start">
+                                   
+                                    </div>
+                                    <a href="#" className="text-sm text-blue-700 hover:underline ml-auto dark:text-blue-500">Esqueceu a Senha?</a>
+                                </div>
+                               <Button>{'Enviar'}</Button>
+                                <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
+                                    Deseja se Cadastrar? <a href="/Acess/account" className="text-blue-700 hover:underline dark:text-blue-500">
+                                        account</a>
+                        </div>
+                </form>
+                </Card>
+                <pre className=' text-black'>{output}</pre>
+             </div>
+       
+        </div>
+    )
+}
